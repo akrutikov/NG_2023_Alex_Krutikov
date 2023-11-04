4b389b73cab1d6d2193c7894f17e8c57ab5221ae
akrutikov <sanakrutikov9@gmail.com>
2023-11-04 20:20:37 +0200

    Cleaned up the extra lines

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,9 +1,8 @@
 perev = input("Enter what you want to convert to degrees Celsius or Fahrenheit? Enter C or F: ")
+temp = float(input("Enter the temperature: "))
 if perev == 'C' or perev == 'c':
-    temp = int(input("Enter the temperature in Сelsius: "))
     print(temp * 9 / 5 + 32, "F")
 elif perev == 'F' or perev == 'f':
-    temp = int(input("Enter the temperature in Fahrenheit: "))
     print((temp -32) * 5 / 9, "C")
 else:
     print("Enter C (Celsius) or F (Fahrenheit)")
