a4732398cfd63729a5057f185609c97f26e0000d
akrutikov <sanakrutikov9@gmail.com>
2023-11-22 18:36:56 +0200

    simplified the program

--- Lection 2/Task 1/main.py
+++ Lection 2/Task 1/main.py
@@ -1,7 +1,2 @@
 element = input("enter any elements: ")
-unique = []
-
-for index in element:
-    if index not in unique:
-        unique.append(index)
-print("Unique elements: ", unique)
+print("Unique elements: ", set(element))
