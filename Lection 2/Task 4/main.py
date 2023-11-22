eb153ee0d411714324628ec6b46b8086a96618d2
akrutikov <sanakrutikov9@gmail.com>
2023-11-22 18:58:43 +0200

    changed the name of the variable

--- Lection 2/Task 4/main.py
+++ Lection 2/Task 4/main.py
@@ -1,7 +1,7 @@
 text = input("Entrr your text: ")
 vowel = "AEIOUYaeiouy"
 letter = []
-for i in text:
-    if i in vowel:
-        letter.append(i)
+for index in text:
+    if index in vowel:
+        letter.append(index)
 print(letter)
