f184df17096843384ff0d0bc8bb4c66c452b1c49
akrutikov <sanakrutikov9@gmail.com>
2023-11-22 19:18:23 +0200

    cleaned up the redundant list

--- Lection 2/Task 2/main.py
+++ Lection 2/Task 2/main.py
@@ -1,11 +1,12 @@
 element = input("Enter any elements: ")
+number = "1234567890"
 values = []
 counter = 0
 
 for index in element:
-        if index.isdigit() == True:
-            values.append(index)
-            counter += 1
+    if index in number:
+        values.append(index)
+        counter += 1
             
 print("=" * 30)            
 print("Your numbers: ", values)
